7fdb369f9993d4c60cda286a9a6b5a6aeeda7356
Benedikt Ritter <beneritter@gmail.com>
2015-08-16 11:51:17 +0200

    Use eslint-loader for processing files with eslint via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
         path: __dirname + '/dist/',
         filename: 'app.js'
     },
+    preLoaders: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'eslint-loader'
+        }
+    ],
+    eslint: {
+        configFile: './.eslintrc'
+    },
     devtool: 'source-map'
 };
