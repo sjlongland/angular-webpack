019992ea666aaf1ab8e5a15440a054b6779e8ad9
Benedikt Ritter <beneritter@gmail.com>
2015-08-16 13:13:48 +0200

    Correct webpack-dev-server config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
     entry: './app.js',
     output: {
         path: path.join(__dirname, 'app/dist'),
-        filename: 'app.js'
+        filename: 'app.js',
+        publicPath: '/dist/'
     },
     module: {
         preLoaders: [
