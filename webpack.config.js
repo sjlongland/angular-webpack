f10d897f7411d19831e4550224c5eacd6dfca881
Stuart Longland <me@vk4msl.id.au>
2019-02-01 15:25:52 +1000

    webpack.config.js: Re-work build system
    - Generate `dist/index.html` from `app/index.html`
    - Copy across included CSS and resources from JavaScript files
    - Put output files in `dist/`

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-var path = require('path');
+"use strict";
+// vim: set tw=78 et sw=4 ts=4 fileencoding=utf-8:
+const path = require('path'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    context: path.join(__dirname, 'app/js'),
-    entry: './app.js',
+    context: path.join(__dirname, 'app'),
+    entry: './js/app.js',
     output: {
-        path: path.join(__dirname, 'app/dist'),
-        filename: 'app.js',
-        publicPath: '/dist/'
+        path: path.join(__dirname, 'dist')
     },
     module: {
         rules: [
@@ -24,15 +26,40 @@ module.exports = {
             },
             {
                 test: /\.html$/,
+                exclude: /index.html$/,
                 use: [
                     'html-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    "css-loader"
                 ]
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Minimal AngularJS application example',
+            template: 'index.html',
+            inject: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
     devtool: 'source-map',
     mode: 'production',
     devServer: {
-        contentBase: path.join(__dirname, 'app')
+        contentBase: path.join(__dirname, 'dist')
     }
 };
